5a2b5636ba9f3d203ea710e44dd2dbe3d55e2097
Yuto Tejima <>
2025-09-19 18:37:30 +0900

    タスクをサーバー上にポストできるようにした

--- src/Entity/TaskEntity.ts
+++ src/Entity/TaskEntity.ts
@@ -2,10 +2,10 @@ export interface TaskEntity {
 	id: string;
 	title: string;
 	description?: string;
-	states: 'pending' | 'in_progress' | 'completed' | 'cancelled';
+	status: 'pending' | 'in_progress' | 'completed' | 'cancelled';
 	priority?: 'low' | 'medium' | 'high' | 'urgent';
-	tags: string[];
-	startedAt?: string;
+	tags?: string[];
+	// startedAt?: string;
 	expiresAt?: string;
 	completedAt?: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,16 @@
-import { TaskEntity } from './Entity/TaskEntity';
 import { Hono } from 'hono';
+import { taskRoute } from './Route/TaskRoute';
+import { cors } from 'hono/cors';
 
 const app = new Hono<{ Bindings: Env }>();
-
-app.get('/api/tasks', async (context) => {
-	const list = await context.env.KV_TASKS.list();
-	const tasks: TaskEntity[] = [];
-
-	for (const key of list.keys) {
-		console.log(key.name);
-		const task = await context.env.KV_TASKS.get<TaskEntity>(key.name, 'json');
-
-		if (!task) {
-			continue;
-		}
-		tasks.push(task);
-	}
-	return context.json(tasks);
-});
-
-app.get('api/tasks/:id', async (context) => {
-	const id = context.req.param('id');
-	const task = await context.env.KV_TASKS.get(id, 'json');
-
-	if (!task) {
-		return context.json({ message: 'タスクが見つかりません' }, 404);
-	}
-	console.log(task);
-	return context.json(task);
-});
-
-app.post('/api/tasks', async (context) => {
-	const id = crypto.randomUUID();
-	const body = await context.req.json();
-
-	const task: TaskEntity = { id, ...body };
-
-	console.log(task);
-	await context.env.KV_TASKS.put(id, JSON.stringify(task));
-
-	return context.json(task);
-});
-
-app.put('/api/tasks/:id', async (context) => {
-	const id = context.req.param('id');
-	const body = await context.req.json();
-	const task = { id, ...body };
-
-	await context.env.KV_TASKS.put(id, JSON.stringify(task));
-
-	return context.json(task);
-});
-
-app.delete('/api/tasks/:id', async (context) => {
-	const id = context.req.param('id');
-	await context.env.KV_TASKS.delete(id);
-
-	return context.json({ message: `タスクid:(${id})を削除しました` });
-});
+app
+	.use(
+		'/api/*',
+		cors({
+			origin: ['http://localhost:5173'],
+		})
+	)
+	.route('/api/v1/tasks', taskRoute);
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
